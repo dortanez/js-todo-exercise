27ef23a7d0b6061a69aed591c5f7a83250d1b5cd
Danisa Ortanez <dortanez@yahoo.com>
2020-08-28 15:21:40 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds decrements the value and console.logs it. Once the value is 0 it should log “DONE!” and stop.
+
+function countDown(num) {
+
+}
