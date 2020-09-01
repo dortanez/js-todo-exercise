11af69f2f300d0df33b64e74aeb72e7862ecca44
Danisa Ortanez <dortanez@yahoo.com>
2020-08-31 22:22:36 -0700

    first commit

--- main.js
+++ main.js
@@ -1,37 +1,44 @@
-// Write a function called countdown that accepts a number as a parameter and every 1000 milliseconds decrements the value and console.logs it. Once the value is 0 it should log “DONE!” and stop.
+const todoInput = document.querySelector('input[type="text"]');
+const todoForm = document.querySelector('#todoForm');
+const todoList = document.querySelector('#todoList');
 
-function countDown(num) {
-    let timer = setInterval(function() {
-        if(num <  0) {
-            console.log('DONE!');
-            clearInterval(timer);
-        } else {
-            console.log(num)
-            num--;
-        } 
-    },1000)
+todoListStorage = [];
+
+todoForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const newItem = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'X';
+    newItem.innerText = todoInput.value + ' ';
+    newItem.appendChild(deleteBtn);
+    todoList.appendChild(newItem);
+    todoInput.value = '';
+    todoListStorage.push(newItem.innerHTML)
+    localStorage.setItem('tasks',JSON.stringify(todoListStorage))
+})
+
+const tasks = JSON.parse(localStorage.getItem('tasks'))
+for(let task of tasks) {
+    todoListStorage.push(task)
+    var newLi = document.createElement('li');
+    newLi.innerHTML = task;
+    todoList.appendChild(newLi);
 }
 
-countDown(5);
-
-
-//Write a function called randomGame that selects a random number between 0 and 1 every 1000 milliseconds and each time that a random number is picked, add 1 to a counter. If the number is greater than .75, stop the timer and console.log the number of tries it took before we found a number greater than .75.
-
-function randomGame() {
-    let counter = 0;
-    let timer = setInterval(function() {
-        let num = Math.random().toFixed(2);
-        if(num < .75) {
-            console.log(num);
-            counter++;
-        } else {
-            counter++;
-            clearInterval(timer);
-            console.log(num);
-            console.log('Number of tries it took to find a number greater than .75:',counter);
+todoList.addEventListener('click', function(e) {
+    if(e.target.tagName === 'BUTTON') {
+        e.target.parentElement.remove();
+        for(let t of todoListStorage) {
+            if(todoListStorage.indexOf(e.target.parentElement.innerHTML) !== -1) {
+                todoListStorage.splice(todoListStorage.indexOf(e.target.parentElement.innerHTML),1)
+            }
         }
-    },1000)
+    }
     
-}
+    if(e.target.tagName === 'LI') {
+        e.target.classList.toggle('strike');
+    }
+})
 
-randomGame();
+console.log(todoListStorage)
+console.log(localStorage.getItem('tasks'))
