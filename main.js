8282808ab78153cf98386125778ee67b4b709898
Danisa Ortanez <dortanez@yahoo.com>
2020-09-01 22:53:03 -0700

    finishing

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const todoInput = document.querySelector('input[type="text"]');
 const todoForm = document.querySelector('#todoForm');
 const todoList = document.querySelector('#todoList');
-const lis = document.querySelectorAll('li');
 
 const todoListStorage = [];
 
@@ -15,35 +14,41 @@ todoForm.addEventListener('submit', function(e) {
     newItem.appendChild(deleteBtn);
     todoList.appendChild(newItem);
     todoInput.value = '';
-    todoListStorage.push(newItem.innerHTML)
+    todoListStorage.push(newItem.innerHTML);
     localStorage.setItem('tasks',JSON.stringify(todoListStorage))
 })
-
 
 // deleting task from page and from storage array
 todoList.addEventListener('click', function(e) {
     if(e.target.tagName === 'BUTTON') {
         e.target.parentElement.remove();
-        console.log(todoListStorage.indexOf(e.target.parentElement.innerHTML))
-        console.log(e.target.parentElement.innerHTML)
-        console.log(todoListStorage)
         todoListStorage.splice(todoListStorage.indexOf(e.target.parentElement.innerHTML),1)
         localStorage.setItem('tasks',JSON.stringify(todoListStorage))       
     } 
+    // completing task when clicked
     if(e.target.tagName === 'LI') {
+        let old = e.target.innerHTML;
         e.target.classList.toggle('strike');
-    }
+        e.target.children[0].classList.toggle('completed')
+        if(e.target.children[0].classList == 'completed') {
+            todoListStorage[todoListStorage.indexOf(old)] = e.target.innerHTML;
+        } else {
+            e.target.children[0].classList == '';
+            todoListStorage[todoListStorage.indexOf(old)] = e.target.innerHTML;
+        }   
+    } 
+    localStorage.setItem('tasks',JSON.stringify(todoListStorage))    
 })
 
-// getting tasks from local storage and adding it to new page
-const tasks = JSON.parse(localStorage.getItem('tasks'))
+// adding local storage tasks to new page
+let tasks = JSON.parse(localStorage.getItem('tasks'))
 
 for(let task of tasks) {
     const newLi = document.createElement('li');
     newLi.innerHTML = task;
+   if(newLi.children[0].classList == 'completed') {
+       newLi.classList = 'strike';
+   }
     todoList.appendChild(newLi);
     todoListStorage.push(newLi.innerHTML)
 }
-
-
-
